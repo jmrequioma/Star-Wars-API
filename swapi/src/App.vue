89fa0f5b5c3f57301ffd38a53d4074184d774f67
j.requioma <j.requioma@arcanys.com>
2022-03-01 10:36:11 +0800

    Fix spacing and remove unused file

--- swapi/src/App.vue
+++ swapi/src/App.vue
@@ -1,9 +1,9 @@
 <template>
 	<v-app>
-    <v-main class="pa-5">
-      <HomeView />
-    </v-main>
-  </v-app>
+        <v-main class="pa-5">
+            <HomeView />
+        </v-main>
+    </v-app>
 </template>
 
 <script setup lang="ts">
